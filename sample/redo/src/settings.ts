947b25ec735dc9ed665298fb1a09b02a5f6efb04
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-06-09 14:59:18 -0700

    Sample: Baby steps
    This does a query on startup. This is not the right way to do this but
    it shows that things work.

---
+++ sample/redo/src/settings.ts
@@ -0,0 +1,5 @@
+export const diffServerURL = 'https://serve.replicache.dev/pull';
+export const diffServerAuth = '1';
+export const loginURL = 'https://replicache-sample-todo.now.sh/serve/login';
+export const batchURL =
+  'https://replicache-sample-todo.now.sh/serve/replicache-batch';

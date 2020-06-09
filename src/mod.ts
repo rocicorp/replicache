0e4bf598aad9881070673301a22334f995a84ae6
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-06-09 09:28:48 -0700

    Use mode library for typedoc
    This requires typedoc@next but it improves the documentation structure
    significantly.
    
    Use default export for Replicache

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-export {default, default as Replicache} from './replicache.js';
+export {default} from './replicache.js';
 export type {ScanItem} from './scan-item.js';
 export type {ScanId} from './scan-id.js';
 export type {ScanBound} from './scan-bound.js';

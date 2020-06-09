0d5da85eac0e0370bd20711bafa08496e1a8df20
Erik Arvidsson <arv@roci.dev>
2020-06-09 14:34:23 -0700

    Initial sample (#23)
    This was created with `create-react-app` and then stripping some cruft.
    
    Right now it includes Replicache using a `file:` dependency in
    package.json. This is very similar to how things will work with npm
    eventually.

---
+++ sample/redo/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Replicache from 'replicache';
+
+function App() {
+  console.log(Replicache);
+  return (
+    <div className="App">
+      <header className="App-header">Hello from Replicache!</header>
+    </div>
+  );
+}
+
+export default App;

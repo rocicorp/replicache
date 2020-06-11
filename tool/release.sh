20a29702c81e1a2bd43e5834e5c31a89ac3ac7dd
Aaron Boodman <aaron@aaronboodman.com>
2020-06-10 14:15:52 -1000

    Add a release shell script that hacks Cargo.toml to get back lto, reducing size of wasm bundle.
    replicache_client.js        : 4207
    replicache_client.js.br	    : 1184
    replicache_client_bg.wasm   : 15786
    replicache_client_bg.wasm.br: 6859

---
+++ tool/release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT=$DIR/../
+
+(
+    cd $ROOT
+
+    # Grumble. Having both these here disables lto, which makes the bundles
+    # significantly (~23% at time of writing) bigger. This is due to a bug
+    # in Cargo: https://github.com/rust-lang/rust/issues/51009.
+    #
+    # But we need cdylib in order to build wasm, and we need
+    # rlib in order to unit test in wasm:
+    # https://github.com/rust-lang/cargo/issues/6659.
+    #
+    # So hack hack hack for now.
+    sed -i .bak 's/crate-type = \["cdylib", "rlib"\]/crate-type = ["cdylib"]/' Cargo.toml
+
+    rm -rf pkg
+    wasm-pack build --release -t web
+    brotli pkg/replicache_client* -f
+
+    mv Cargo.toml.bak Cargo.toml
+)

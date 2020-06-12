7dea4e719c20161c748727065d5e50a3084ec25f
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-06-12 15:38:48 -0700

    Add drag to reorder
    This uses react-beautiful-drag for reordering
    
    There are some other subtle fixes related to how the Replicacahe object
    is stored so that we do not end up creating two instances.

--- sample/redo/src/List.tsx
+++ sample/redo/src/List.tsx
@@ -1,19 +1,124 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import type {Todo, MutationFunctions} from './App';
 import {ListItem} from './ListItem';
+import {DragDropContext, Droppable} from 'react-beautiful-dnd';
+import type {DropResult} from 'react-beautiful-dnd';
+import type Replicache from '../../../out.cjs/mod';
+import {newOrderBetween} from './order';
 
-export function List({
-  todos,
-  mutations,
-}: {
-  todos: Todo[];
-  mutations: MutationFunctions;
-}) {
+export const List = React.memo(
+  ({
+    todos,
+    mutations,
+    rep,
+  }: {
+    todos: Todo[];
+    mutations: MutationFunctions;
+    rep: Replicache;
+  }) => {
+    const onDragEnd = useCallback(
+      (result: DropResult) => {
+        if (!result.destination) {
+          return;
+        }
+
+        if (result.destination.index === result.source.index) {
+          return;
+        }
+
+        handleReorder(
+          result.source.index,
+          result.destination.index,
+          todos,
+          rep,
+          mutations.updateTodo,
+        );
+
+        // Also mutate the array. If we do not do this we flicker a bit... It
+        // seems like this is required by react-beautiful-dnd.
+        const [removed] = todos.splice(result.source.index, 1);
+        todos.splice(result.destination.index, 0, removed);
+      },
+      [todos, rep, mutations],
+    );
+
+    return (
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="todos">
+          {provided => (
+            <ul
+              className="App-list"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {todos.map((todo, index) => (
+                <ListItem
+                  key={todo.id}
+                  todo={todo}
+                  mutations={mutations}
+                  index={index}
+                />
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  },
+  (prevProps, nextProps) => {
+    const prevTodos = prevProps.todos;
+    const nextTodos = nextProps.todos;
+    return (
+      prevTodos.length === nextTodos.length &&
+      prevTodos.every((t1, i) => todosEqual(t1, nextTodos[i]))
+    );
+  },
+);
+
+function todosEqual(t1: Todo, t2: Todo): boolean {
   return (
-    <ul className="App-list">
-      {todos.map(todo => (
-        <ListItem key={todo.id} todo={todo} mutations={mutations} />
-      ))}
-    </ul>
+    t1.id === t2.id &&
+    t1.listId === t2.listId &&
+    t1.text === t2.text &&
+    t1.complete === t2.complete &&
+    t1.order === t2.order
   );
+}
+
+async function handleReorder(
+  oldIndex: number,
+  newIndex: number,
+  todos: Todo[],
+  rep: Replicache,
+  updateTodo: MutationFunctions['updateTodo'],
+) {
+  if (oldIndex === newIndex) {
+    return;
+  }
+
+  if (newIndex === todos.length && oldIndex === todos.length - 1) {
+    return;
+  }
+
+  let id = todos[oldIndex].id;
+  let left: Todo | null = null;
+  let right: Todo | null = null;
+  if (newIndex === 0) {
+    right = todos[0];
+  } else if (newIndex === todos.length - 1) {
+    left = todos[todos.length - 1];
+  } else {
+    if (newIndex > oldIndex) {
+      left = todos[newIndex];
+      right = todos[newIndex + 1];
+    } else {
+      left = todos[newIndex - 1];
+      right = todos[newIndex];
+    }
+  }
+
+  const order = newOrderBetween(left, right);
+  await updateTodo({id, order});
+  rep.sync();
 }
--- sample/redo/src/ListItem.tsx
+++ sample/redo/src/ListItem.tsx
@@ -1,12 +1,15 @@
 import React, {useCallback} from 'react';
 import type {Todo, MutationFunctions} from './App';
+import {Draggable} from 'react-beautiful-dnd';
 
 export function ListItem({
   todo,
   mutations,
+  index,
 }: {
   todo: Todo;
   mutations: MutationFunctions;
+  index: number;
 }) {
   const changeCallback = useCallback(() => {
     mutations.updateTodo({id: todo.id, complete: !todo.complete});
@@ -15,16 +18,24 @@ export function ListItem({
     mutations.deleteTodo(todo);
   }, [todo, mutations]);
   return (
-    <li>
-      <label>
-        <input
-          type="checkbox"
-          checked={todo.complete}
-          onChange={changeCallback}
-        />{' '}
-        {todo.text}
-      </label>
-      <button onClick={deleteCallback}>Delete</button>
-    </li>
+    <Draggable draggableId={String(todo.id)} index={index}>
+      {provided => (
+        <li
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={changeCallback}
+            />
+            <span>{todo.text}</span>
+          </label>
+          <button onClick={deleteCallback}>Delete</button>
+        </li>
+      )}
+    </Draggable>
   );
 }

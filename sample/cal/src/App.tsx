a5692ec4fcd2764ab230b2a9f081ec920ee23fbc
Aaron Boodman <aaron@aaronboodman.com>
2020-06-16 22:06:22 -1000

    Refactor Replicache integration into separate file

--- sample/cal/src/App.tsx
+++ sample/cal/src/App.tsx
@@ -2,12 +2,7 @@ import React, {
   useEffect,
   useState
 } from 'react';
-import Replicache, {
-  REPMHTTPInvoker,
-  ReadTransaction,
-  WriteTransaction,
-  Mutator,
-} from 'replicache';
+import type {ReadTransaction, WriteTransaction} from 'replicache';
 import add from 'date-fns/add'
 import setDay from 'date-fns/setDay';
 import areIntervalsOverlapping from 'date-fns/areIntervalsOverlapping';
@@ -15,53 +10,17 @@ import format from 'date-fns/format';
 import uuid from 'uuid-random';
 
 import './App.css';
-import {
-  dataLayerAuth,
-  diffServerURL,
-  diffServerAuth,
-  batchURL,
-} from './settings';
 import type {
-  EventIdentity,
   Event,
-  EventUpdate,
   NormalizedEvent,
   EventDate
 } from './types';
-
-const repmInvoke = new REPMHTTPInvoker('http://localhost:7002').invoke;
-
-const rep = new Replicache({
-  diffServerURL,
-  dataLayerAuth,
-  diffServerAuth,
-  batchURL,
-  repmInvoke,
-});
-
-function eventKey(event: EventIdentity) {
-  return `/event/${event.id}`;
-}
-
-const addEvent = rep.register('addEvent', async (tx: WriteTransaction, event: Event) => {
-  await tx.put(eventKey(event), event);
-});
-
-const updateEvent = rep.register('updateEvent', async (tx: WriteTransaction, update: EventUpdate) => {
-  const event = (await tx.get(eventKey(update))) as Event|null;
-  if (!event) {
-    console.warn(`Possible conflict - event ${update.id} not found`);
-    return;
-  }
-  event.summary = update.summary ?? event.summary;
-  event.start = update.start ?? event.start;
-  event.end = update.end ?? event.end;
-  await tx.put(eventKey(event), event);
-});
-
-const deleteEvent = rep.register('deleteEvent', async (tx: WriteTransaction, id: EventIdentity) => {
-  await tx.del(eventKey(id));
-});
+import {
+  rep,
+  addEvent,
+  updateEvent,
+  deleteEvent,
+} from './data';
 
 export default App;
 
---
+++ sample/cal/src/data.ts
@@ -0,0 +1,52 @@
+import Replicache, {
+  REPMHTTPInvoker,
+  ReadTransaction,
+  WriteTransaction,
+  Mutator,
+} from 'replicache';
+
+import {
+  dataLayerAuth,
+  diffServerURL,
+  diffServerAuth,
+  batchURL,
+} from './settings';
+import type {
+  EventIdentity,
+  Event,
+  EventUpdate,
+} from './types';
+
+const repmInvoke = new REPMHTTPInvoker('http://localhost:7002').invoke;
+
+export const rep = new Replicache({
+  diffServerURL,
+  dataLayerAuth,
+  diffServerAuth,
+  batchURL,
+  repmInvoke,
+});
+
+function eventKey(event: EventIdentity) {
+  return `/event/${event.id}`;
+}
+
+export const addEvent = rep.register('addEvent', async (tx: WriteTransaction, event: Event) => {
+  await tx.put(eventKey(event), event);
+});
+
+export const updateEvent = rep.register('updateEvent', async (tx: WriteTransaction, update: EventUpdate) => {
+  const event = (await tx.get(eventKey(update))) as Event|null;
+  if (!event) {
+    console.warn(`Possible conflict - event ${update.id} not found`);
+    return;
+  }
+  event.summary = update.summary ?? event.summary;
+  event.start = update.start ?? event.start;
+  event.end = update.end ?? event.end;
+  await tx.put(eventKey(event), event);
+});
+
+export const deleteEvent = rep.register('deleteEvent', async (tx: WriteTransaction, id: EventIdentity) => {
+  await tx.del(eventKey(id));
+});

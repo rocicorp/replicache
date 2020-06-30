01b238dc035c891235cfe9f271778a98583db1b3
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-06-30 14:53:03 -0700

    Run prettier on samples

--- sample/cal/src/data.ts
+++ sample/cal/src/data.ts
@@ -11,11 +11,7 @@ import {
   diffServerAuth,
   batchURL,
 } from './settings';
-import type {
-  EventIdentity,
-  Event,
-  EventUpdate,
-} from './types';
+import type {EventIdentity, Event, EventUpdate} from './types';
 
 const repmInvoke = new REPMHTTPInvoker('http://localhost:7002').invoke;
 
@@ -31,22 +27,31 @@ function eventKey(event: EventIdentity) {
   return `/event/${event.id}`;
 }
 
-export const addEvent = rep.register('addEvent', async (tx: WriteTransaction, event: Event) => {
-  await tx.put(eventKey(event), event);
-});
+export const addEvent = rep.register(
+  'addEvent',
+  async (tx: WriteTransaction, event: Event) => {
+    await tx.put(eventKey(event), event);
+  },
+);
 
-export const updateEvent = rep.register('updateEvent', async (tx: WriteTransaction, update: EventUpdate) => {
-  const event = (await tx.get(eventKey(update))) as Event|null;
-  if (!event) {
-    console.warn(`Possible conflict - event ${update.id} not found`);
-    return;
-  }
-  event.summary = update.summary ?? event.summary;
-  event.start = update.start ?? event.start;
-  event.end = update.end ?? event.end;
-  await tx.put(eventKey(event), event);
-});
+export const updateEvent = rep.register(
+  'updateEvent',
+  async (tx: WriteTransaction, update: EventUpdate) => {
+    const event = (await tx.get(eventKey(update))) as Event | null;
+    if (!event) {
+      console.warn(`Possible conflict - event ${update.id} not found`);
+      return;
+    }
+    event.summary = update.summary ?? event.summary;
+    event.start = update.start ?? event.start;
+    event.end = update.end ?? event.end;
+    await tx.put(eventKey(event), event);
+  },
+);
 
-export const deleteEvent = rep.register('deleteEvent', async (tx: WriteTransaction, id: EventIdentity) => {
-  await tx.del(eventKey(id));
-});
+export const deleteEvent = rep.register(
+  'deleteEvent',
+  async (tx: WriteTransaction, id: EventIdentity) => {
+    await tx.del(eventKey(id));
+  },
+);

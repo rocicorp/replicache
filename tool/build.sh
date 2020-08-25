c9880ac4be62f0b1dabae3c4636f2ed451cac0bb
Aaron Boodman <aaron@aaronboodman.com>
2020-08-25 13:35:02 -1000

    Add REPMWASMInvoker to the Replicache JS SDK.

--- tool/build.sh
+++ tool/build.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 ORIG=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/../
 REPM_VERSION='v0.11.0'
-
-echo "Fetching test-server..."
+REPC_VERSION='v0.1.0'
 
 (
   cd $ROOT
@@ -21,13 +21,28 @@ echo "Fetching test-server..."
   # Repm is an implementation detail of bindings, we do not maintain
   # backward compatibility. We need to download the correct version
   # this SDK works with.
+  echo "Fetching test-server..."
   URL="https://github.com/rocicorp/replicache-client/releases/download/$REPM_VERSION/test-server-amd64-$PLATFORM"
   curl -L -f $URL > bin/test-server
   chmod u+x bin/test-server
 
   # Diff server is a public interface. We *do* maintain backward compat.
   # We download the latest release.
+  echo "Fetching diff-server..."
   URL="https://github.com/rocicorp/diff-server/releases/latest/download/diffs-$PLATFORM"
   curl -L -f $URL > bin/diff-server
   chmod u+x bin/diff-server
+
+  echo "Fetching repc..."
+  URL="https://github.com/rocicorp/repc/releases/download/$REPC_VERSION/repc.zip"
+  curl -L -f $URL > bin/repc.zip
+  cd bin
+  rm -rf repc
+  unzip repc.zip
+  mv pkg repc
+  rm repc.zip
+  
+  cd ../src/
+  rm wasm
+  ln -s ../bin/repc wasm
 )

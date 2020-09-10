b13fa27086892c0a8064e6efa1bdaee90c2e84f5
Aaron Boodman <aaron@aaronboodman.com>
2020-09-10 13:47:59 -1000

    Roll repc to 0.6

--- tool/get-deps.sh
+++ tool/get-deps.sh
@@ -3,7 +3,7 @@ ORIG=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/../
 REPM_VERSION='v0.11.0'
-REPC_VERSION='v0.5.0'
+REPC_VERSION='v0.6.0'
 
 (
   cd $ROOT

8a7e47ff08c7677856bd675633a42c5ed19558b1
github-action-benchmark <github@users.noreply.github.com>
2020-09-25 20:51:37 +0000

    add Benchmark (benchmarkjs) benchmark result for 877824a711579bdb4ae3057b7289489956ae9ea5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,1 +1,56 @@
-window.BENCHMARK_DATA = {};
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1601067097742,
+  "repoUrl": "https://github.com/rocicorp/replicache-sdk-js",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "rocicorp",
+            "username": "rocicorp"
+          },
+          "committer": {
+            "name": "rocicorp",
+            "username": "rocicorp"
+          },
+          "id": "877824a711579bdb4ae3057b7289489956ae9ea5",
+          "message": "Benchmark test Again",
+          "timestamp": "2020-09-24T00:39:50Z",
+          "url": "https://github.com/rocicorp/replicache-sdk-js/pull/113/commits/877824a711579bdb4ae3057b7289489956ae9ea5"
+        },
+        "date": 1601067097003,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "populate 1024x1000 (clean)",
+            "value": 1.49,
+            "range": "±0.0%",
+            "unit": "MB/s",
+            "extra": "0 samples"
+          },
+          {
+            "name": "populate 1024x1000 (dirty)",
+            "value": 1.21,
+            "range": "±0.0%",
+            "unit": "MB/s",
+            "extra": "0 samples"
+          },
+          {
+            "name": "scan 1024x1000",
+            "value": 46.5,
+            "range": "±0.0%",
+            "unit": "MB/s",
+            "extra": "0 samples"
+          },
+          {
+            "name": "scan 1024x5000",
+            "value": 38.75,
+            "range": "±0.0%",
+            "unit": "MB/s",
+            "extra": "0 samples"
+          }
+        ]
+      }
+    ]
+  }
+}

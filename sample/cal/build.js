bcbbd1f3b491292fb44e9603a7164cd66588e0dc
Erik Arvidsson <erik.arvidsson@gmail.com>
2020-12-16 14:55:23 -0800

    Parallel build

--- sample/cal/build.js
+++ sample/cal/build.js
@@ -1,25 +1,34 @@
-/* eslint-env node */
+/* eslint-env node, es6 */
 
 import esbuild from 'esbuild';
-import {copyFileSync} from 'fs';
+import {copyFile} from 'fs/promises';
 
-copyFileSync(
-  './node_modules/replicache/out/replicache.wasm',
-  'public/replicache.wasm',
-);
+async function main() {
+  const p1 = copyFile(
+    './node_modules/replicache/out/replicache.wasm',
+    'public/replicache.wasm',
+  );
 
-const NODE_ENV = process.env.NODE_ENV ?? '';
-const isDev = NODE_ENV !== 'production';
+  const NODE_ENV = process.env.NODE_ENV ?? '';
+  const isDev = NODE_ENV !== 'production';
 
-esbuild.buildSync({
-  entryPoints: ['src/index.tsx'],
-  define: {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-  },
-  outdir: 'public',
-  format: 'esm',
-  bundle: true,
-  minify: !isDev,
-  sourcemap: isDev,
+  const p2 = esbuild.build({
+    entryPoints: ['src/index.tsx'],
+    define: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    },
+    outdir: 'public',
+    format: 'esm',
+    bundle: true,
+    minify: !isDev,
+    sourcemap: isDev,
+  });
+
+  await Promise.all([p1, p2]);
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(42);
 });

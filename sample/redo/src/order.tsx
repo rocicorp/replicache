8da53e8c655dddc7dbaf847a286c684c41f39289
Erik Arvidsson <arv@roci.dev>
2020-12-16 09:55:53 -0800

    Update sample/redo to 4.0.0 (#244)

--- sample/redo/src/order.tsx
+++ sample/redo/src/order.tsx
@@ -1,24 +1,10 @@
 import type {Todo} from './App';
+import {generateKeyBetween} from 'fractional-indexing';
 
-export function newOrder(before: number | null, after: number | null) {
-  const minOrderValue = 0;
-  const maxOrderValue = Number.MAX_SAFE_INTEGER;
-  if (before === null) {
-    before = minOrderValue;
-  }
-  if (after === null) {
-    after = maxOrderValue;
-  }
-  return before + (after - before) / 2;
-}
-/**
- * calculates the order field by halving the distance between the left and right
- * neighbor orders.
- * min default value = -minPositive
- * max default value = Number.MAX_SAFE_INTEGER
- */
-export function newOrderBetween(left: Todo | null, right: Todo | null) {
+export {generateKeyBetween as newOrder};
+
+export function newOrderBetween(left: Todo | null, right: Todo | null): string {
   const leftOrder = left?.order ?? null;
   const rightOrder = right?.order ?? null;
-  return newOrder(leftOrder, rightOrder);
+  return generateKeyBetween(leftOrder, rightOrder);
 }

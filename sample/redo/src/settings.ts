331f43773f3c11c130c926a463b7f7b8dbda6cb2
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-02-01 12:25:07 -0800

    Migrate redo to 5.0.0

--- sample/redo/src/settings.ts
+++ sample/redo/src/settings.ts
@@ -1,5 +1,7 @@
 export const diffServerURL = 'https://serve.replicache.dev/pull';
 export const diffServerAuth = '1';
-export const loginURL = 'https://replicache-sample-todo.now.sh/serve/login';
-export const batchURL =
-  'https://replicache-sample-todo.now.sh/serve/replicache-batch';
+
+const dataLayerBase = 'https://replicache-sample-todo.now.sh/serve/';
+export const loginURL = `${dataLayerBase}login`;
+export const batchURL = `${dataLayerBase}replicache-batch`;
+export const clientViewURL = `${dataLayerBase}replicache-client-view`;

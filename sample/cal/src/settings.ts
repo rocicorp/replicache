0186c096d5ab575a034c4de65465d1cfd451778e
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-02-01 12:53:57 -0800

    Migrate cal to 5.0.0

--- sample/cal/src/settings.ts
+++ sample/cal/src/settings.ts
@@ -1,5 +1,9 @@
-export const diffServerURL = 'http://localhost:7001/pull';
+export const diffServerURL = 'https://serve.replicache.dev/pull';
 export const diffServerAuth = 'sandbox';
-export const batchURL = 'http://localhost:5000/replicache-batch';
+
+const dataLayerBase = 'https://replicache-sample-gcal.herokuapp.com/';
+export const batchURL = `${dataLayerBase}replicache-batch`;
+export const clientViewURL = `${dataLayerBase}replicache-client-view`;
+
 export const dataLayerAuth =
   'Bearer ya29.a0AfH6SMAkrScImvQkxVMMDwCVz-NriDInA10pK4z5s4tOvoS_dEUQ666KvI5TFUF7KnMxwVIarDrlrn1G3Vt0ozc4w3q-2nf7Z2wjPd3HUXPzVtWBYQhva8IK7MClva1Cjg4Sdw2dc3BT5nRCWRS6KKlr-CglpVOis-Y';

4ec3c564a189c637a872ad2d1875877869b700ce
Erik Arvidsson <arv@roci.dev>
2021-03-12 12:45:03 -0800

    Delete all the sample (#306)
    https://i.imgflip.com/5113yn.jpg

--- sample/cal/README.md
+++
@@ -1,8 +0,0 @@
-This is a sample app for Replicache.
-
-Use `npm run build` to create optimized version.
-
-Use `npm run start` to start a web server.
-
-Use `npm run dev` to create dev version and start web server. Changes to src
-gets recompiled but you still have to reload the webpage manually.
--- sample/redo/src/settings.ts
+++
@@ -1,7 +0,0 @@
-export const diffServerURL = 'https://serve.replicache.dev/pull';
-export const diffServerAuth = '1';
-
-const dataLayerBase = 'https://replicache-sample-todo.now.sh/serve/';
-export const loginURL = `${dataLayerBase}login`;
-export const batchURL = `${dataLayerBase}replicache-batch`;
-export const clientViewURL = `${dataLayerBase}replicache-client-view`;

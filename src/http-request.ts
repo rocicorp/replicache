c52be6b0a466ab087bb69963ecc709a85d7481ab
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-06-17 16:40:30 -0700

    cleanuo

--- src/http-request.ts
+++ src/http-request.ts
@@ -1,23 +1,22 @@
 import type {JSONValue} from './json.js';
 import type {HTTPRequestInfo} from './repm-invoker.js';
 
-function getHeaders(arg: {auth: string; requestID: string}): {
-  [key: string]: string;
-} {
+/**
+ * This is used be the defaultPuller and defaultPusher to reduce code
+ * duplication.
+ */
+export async function httpRequest(
+  arg: {url: string; auth: string; requestID: string},
+  body: JSONValue,
+): Promise<{httpRequestInfo: HTTPRequestInfo; response: Response}> {
   /* eslint-disable @typescript-eslint/naming-convention */
-  return {
+  const headers = {
     'Content-type': 'application/json',
     Authorization: arg.auth,
     'X-Replicache-RequestID': arg.requestID,
   };
   /* eslint-disable @typescript-eslint/naming-convention */
-}
 
-export async function httpRequest(
-  arg: {url: string; auth: string; requestID: string},
-  body: JSONValue,
-): Promise<{httpRequestInfo: HTTPRequestInfo; response: Response}> {
-  const headers = getHeaders(arg);
   const response = await fetch(arg.url, {
     method: 'POST',
     headers,

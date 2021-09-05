rocicorp/replicache

commit 48c699c27abf62565360a771d4fb935f7ab8f95a

Store JS Values in IDB (#470)
* Store JS Values in IDB

This contains a few changes that were all related.

- Change keys to `string`s (both for `prolly.Map` and `kv.Store`)
- Removes the flatbuffer for prolly/Leaf and uses JSON instead.
- Changes `kv.Value` to `JSONValue | Uint8Array`
- Changes `prolly.Map` value to `JSONValue` (this has wide implications throughout)
- IDB now stores `JSONValue | Uint8Array` directly

* No need to JSON stringify console log

* Cleanup

src/kv/write-impl-base.ts
@@ -1,11 +1,10 @@
-import type {Read} from './store';
+import type {Read, Value} from './store';
 
-export const deleteSentinel = null;
+export const deleteSentinel = Symbol();
 export type DeleteSentinel = typeof deleteSentinel;
 
 export class WriteImplBase {
-  protected readonly _pending: Map<string, Uint8Array | DeleteSentinel> =
-    new Map();
+  protected readonly _pending: Map<string, Value | DeleteSentinel> = new Map();
   private readonly _read: Read;
 
   constructor(read: Read) {
@@ -23,7 +22,7 @@ export class WriteImplBase {
     }
   }
 
-  async get(key: string): Promise<Uint8Array | undefined> {
+  async get(key: string): Promise<Value | undefined> {
     const v = this._pending.get(key);
     switch (v) {
       case deleteSentinel:
@@ -35,7 +34,7 @@ export class WriteImplBase {
     }
   }
 
-  async put(key: string, value: Uint8Array): Promise<void> {
+  async put(key: string, value: Value): Promise<void> {
     this._pending.set(key, value);
   }
 

src/prolly/map.ts
@@ -1,20 +1,25 @@
 import type * as dag from '../dag/mod';
+import {deepEqual, JSONValue} from '../json';
 import {arrayCompare} from './array-compare';
 import {Leaf} from './leaf';
 import {PeekIterator} from './peek-iterator';
 import {stringCompare} from './string-compare';
-import * as utf8 from '../utf8';
 
-// TODO(arv): Use string for key instead of Uint8Array
-export type Entry = [key: Uint8Array, value: Uint8Array];
+export type Entry = [key: string, value: JSONValue];
+
+export const deleteSentinel = Symbol();
+export type DeleteSentinel = typeof deleteSentinel;
 
 class ProllyMap {
   private _base: Leaf | undefined;
   // TODO: Should really be a BTreeMap because we want ordering... Will do hacky sort
   // for now.
-  private readonly _pending: Map<string, Uint8Array | null>;
+  private readonly _pending: Map<string, JSONValue | DeleteSentinel>;
 
-  constructor(base: Leaf | undefined, pending: Map<string, Uint8Array | null>) {
+  constructor(
+    base: Leaf | undefined,
+    pending: Map<string, JSONValue | DeleteSentinel>,
+  ) {
     this._base = base;
     this._pending = pending;
   }
@@ -32,29 +37,27 @@ class ProllyMap {
     return new ProllyMap(base, new Map());
   }
 
-  has(key: Uint8Array): boolean {
-    const ks = utf8.decode(key);
-    const p = this._pending.get(ks);
+  has(key: string): boolean {
+    const p = this._pending.get(key);
     if (p !== undefined) {
-      // if null the key was deleted.
-      return p !== null;
+      return p !== deleteSentinel;
     }
 
     return this._baseHas(key);
   }
 
-  private _baseHas(key: Uint8Array): boolean {
+  private _baseHas(key: string): boolean {
     if (this._base === undefined) {
       return false;
     }
     return this._base.binarySearch(key).found;
   }
 
-  get(key: Uint8Array): Uint8Array | undefined {
-    const ks = utf8.decode(key);
+  get(key: string): JSONValue | undefined {
+    const ks = key;
     const p = this._pending.get(ks);
     switch (p) {
-      case null:
+      case deleteSentinel:
         return undefined;
       case undefined:
         return this._baseGet(key);
@@ -63,7 +66,7 @@ class ProllyMap {
     }
   }
 
-  private _baseGet(key: Uint8Array): Uint8Array | undefined {
+  private _baseGet(key: string): JSONValue | undefined {
     if (this._base === undefined) {
       return undefined;
     }
@@ -71,19 +74,15 @@ class ProllyMap {
     if (!found) {
       return undefined;
     }
-    return this._base.getEntryByIndex(index).valArray() ?? undefined;
+    return this._base.entries[index][1];
   }
 
-  put(key: Uint8Array, val: Uint8Array): void {
-    // TODO(arv): Consider storing the Uint8Array key in the value if we want to
-    // keep using Uint8Array keys.
-    const ks = utf8.decode(key);
-    this._pending.set(ks, val);
+  put(key: string, val: JSONValue): void {
+    this._pending.set(key, val);
   }
 
-  del(key: Uint8Array): void {
-    const ks = utf8.decode(key);
-    this._pending.set(ks, null);
+  del(key: string): void {
+    this._pending.set(key, deleteSentinel);
   }
 
   entries(): IterableIterator<Entry> {
@@ -95,7 +94,7 @@ class ProllyMap {
   }
 
   async flush(write: dag.Write): Promise<string> {
-    const newBase = Leaf.new(this);
+    const newBase = new Leaf([...this]);
     await write.putChunk(newBase.chunk);
     this._base = newBase;
     this._pending.clear();
@@ -115,27 +114,27 @@ class ProllyMap {
       }
 
       if (a.done && !b.done) {
-        keys.push(utf8.decode(b.value[0]));
+        keys.push(b.value[0]);
         b = itB.next();
       } else if (!a.done && b.done) {
-        keys.push(utf8.decode(a.value[0]));
+        keys.push(a.value[0]);
         a = itA.next();
       } else if (!a.done && !b.done) {
-        const ord = arrayCompare(a.value[0], b.value[0]);
+        const ord = stringCompare(a.value[0], b.value[0]);
         switch (ord) {
           case -1:
-            keys.push(utf8.decode(a.value[0]));
+            keys.push(a.value[0]);
             a = itA.next();
             break;
           case 0:
-            if (arrayCompare(a.value[1], b.value[1]) !== 0) {
-              keys.push(utf8.decode(a.value[0]));
+            if (!deepEqual(a.value[1], b.value[1])) {
+              keys.push(a.value[0]);
             }
             a = itA.next();
             b = itB.next();
             break;
           case +1:
-            keys.push(utf8.decode(b.value[0]));
+            keys.push(b.value[0]);
             b = itB.next();
             break;
         }
@@ -150,12 +149,13 @@ class ProllyMap {
       stringCompare(a[0], b[0]),
     );
 
-    for (const [key, pendingVal] of entries) {
-      if (pendingVal !== null) {
-        // TODO(arv): Use strings for keys.
-        const baseVal = this._baseGet(utf8.encode(key));
+    for (const entry of entries) {
+      const key = entry[0];
+      const pendingVal = entry[1];
+      if (pendingVal !== deleteSentinel) {
+        const baseVal = this._baseGet(key);
         if (baseVal !== undefined) {
-          if (arrayCompare(baseVal, pendingVal) !== 0) {
+          if (!deepEqual(baseVal, pendingVal)) {
             keys.push(key);
           }
         } else {
@@ -163,7 +163,7 @@ class ProllyMap {
         }
       } else {
         // pending was deleted.
-        if (this._baseHas(utf8.encode(key))) {
+        if (this._baseHas(key)) {
           keys.push(key);
         }
       }
@@ -178,26 +178,25 @@ const emptyIterator: Iterator<Entry> = {
   },
 };
 
-// TODO(arv): Use string here
-type DeletableEntry = [key: Uint8Array, val: Uint8Array | null];
+type DeletableEntry = [key: string, val: JSONValue | DeleteSentinel];
 
 // TODO(arv): Refactor to use generator(s)?
 class Iter implements IterableIterator<Entry> {
   private readonly _base: PeekIterator<Entry>;
   private readonly _pending: PeekIterator<DeletableEntry>;
 
-  constructor(base: Leaf | undefined, pending: Map<string, Uint8Array | null>) {
+  constructor(
+    base: Leaf | undefined,
+    pending: Map<string, JSONValue | DeleteSentinel>,
+  ) {
     this._base = new PeekIterator(
       base ? base[Symbol.iterator]() : emptyIterator,
     );
 
     // Since we do not have a BTreeMap we have to sort the pending entries.
-    const p: [string, Uint8Array | null][] = [...pending];
+    const p: [string, JSONValue | DeleteSentinel][] = [...pending];
     p.sort((a, b) => stringCompare(a[0], b[0]));
-    const entries: DeletableEntry[] = p.map(([key, val]) => [
-      utf8.encode(key),
-      val,
-    ]);
+    const entries: DeletableEntry[] = p.map(([key, val]) => [key, val]);
     this._pending = new PeekIterator(entries.values());
   }
 
@@ -211,7 +210,7 @@ class Iter implements IterableIterator<Entry> {
       if (ni.done) {
         return ni;
       }
-      if (ni.value[1] === null) {
+      if (ni.value[1] === deleteSentinel) {
         // Key was deleted
         continue;
       }

src/prolly/map.test.ts
@@ -4,9 +4,9 @@ import {MemStore} from '../kv/mod';
 import {Leaf} from './leaf';
 import {stringCompare} from './string-compare';
 import * as prolly from './mod';
-import * as utf8 from '../utf8';
-import {b} from '../test-util';
 import {initHasher} from '../hash';
+import type {JSONValue} from '../json';
+import {deleteSentinel, DeleteSentinel} from './map';
 
 setup(async () => {
   await initHasher();
@@ -18,24 +18,22 @@ function makeMap(
   deleted: string[],
 ): prolly.Map {
   const entries = base && base.sort();
-  const leaf =
-    entries && Leaf.new(entries.map(s => [utf8.encode(s), utf8.encode(s)]));
+  const leaf = entries && new Leaf(entries.map(s => [s, s]));
 
   const pm = new Map();
   for (const p of pending) {
-    const v = utf8.encode(p);
-    v.reverse();
+    const v = p.split('').reverse().join('');
     pm.set(p, v);
   }
   for (const p of deleted) {
-    pm.set(p, null);
+    pm.set(p, deleteSentinel);
   }
   return new prolly.Map(leaf, pm);
 }
 
 test('has', () => {
   const t = (map: prolly.Map, test: string, expected: boolean) => {
-    const actual = map.has(utf8.encode(test));
+    const actual = map.has(test);
     expect(actual).to.equal(expected);
   };
 
@@ -68,8 +66,8 @@ test('has', () => {
 
 test('get', async () => {
   const t = (map: prolly.Map, test: string, expected: string | undefined) => {
-    const actual = map.get(utf8.encode(test));
-    expect(actual).to.deep.equal(expected && utf8.encode(expected));
+    const actual = map.get(test);
+    expect(actual).to.deep.equal(expected);
   };
 
   // Empty
@@ -113,9 +111,9 @@ test('put', async () => {
     expected: string | undefined,
   ) => {
     const map = makeMap(base, pending, deleted);
-    map.put(utf8.encode(put), b`x`);
-    const actual = map.get(utf8.encode(put));
-    expect(actual).to.deep.equal(expected && utf8.encode(expected));
+    map.put(put, 'x');
+    const actual = map.get(put);
+    expect(actual).to.deep.equal(expected);
   };
 
   // Empty
@@ -141,8 +139,8 @@ test('del', async () => {
     del: string,
   ) => {
     const map = makeMap(base, pending, deleted);
-    map.del(utf8.encode(del));
-    const has = map.has(utf8.encode(del));
+    map.del(del);
+    const has = map.has(del);
     expect(has).to.be.false;
   };
 
@@ -167,12 +165,11 @@ test('iter flush', async () => {
     base: string[] | undefined,
     pending: string[],
     deleted: string[],
-    expected1: string[],
+    expected: string[],
   ) => {
     const map = makeMap(base, pending, deleted);
-    const expected = expected1.map(utf8.encode);
 
-    const t = (map: prolly.Map, expected: Uint8Array[]) => {
+    const t = (map: prolly.Map, expected: string[]) => {
       const actual = [...map].map(item => item[0]);
       expect(actual).to.deep.equal(expected);
     };
@@ -201,22 +198,22 @@ test('iter flush', async () => {
 
   // Base-only
   await t([], [], [], []);
-  await t([''], [], [], ['']);
-  await t(['', 'foo'], [], [], ['', 'foo']);
+  await t(['0'], [], [], ['0']);
+  await t(['0', 'foo'], [], [], ['0', 'foo']);
 
   // Pending-only
-  await t(undefined, [''], [], ['']);
-  await t(undefined, ['', 'foo'], [], ['', 'foo']);
+  await t(undefined, ['0'], [], ['0']);
+  await t(undefined, ['0', 'foo'], [], ['0', 'foo']);
 
   // basic+pending
-  await t(['', 'foo'], ['bar', 'foo'], [], ['', 'bar', 'foo']);
-  await t([''], ['', 'bar'], [], ['', 'bar']);
+  await t(['0', 'foo'], ['bar', 'foo'], [], ['0', 'bar', 'foo']);
+  await t(['0'], ['0', 'bar'], [], ['0', 'bar']);
   await t(['a', 'b'], ['c', 'd'], [], ['a', 'b', 'c', 'd']);
   await t(['c', 'd'], ['a', 'b'], [], ['a', 'b', 'c', 'd']);
   await t(['b', 'd'], ['a', 'c'], [], ['a', 'b', 'c', 'd']);
 
   // deletes
-  await t([], [], [''], []);
+  await t([], [], ['0'], []);
   await t([], [], ['a'], []);
   await t(['a'], [], ['a'], []);
   await t(['a', 'b'], [], ['a'], ['b']);
@@ -252,35 +249,35 @@ test('changed keys', () => {
 function makeProllyMap(m: Record<string, string>): prolly.Map {
   const entries = Object.entries(m);
   entries.sort((a, b) => stringCompare(a[0], b[0]));
-  const pending: Map<string, Uint8Array | null> = new Map();
+  const pending: Map<string, JSONValue | DeleteSentinel> = new Map();
   for (const [key, value] of entries) {
-    pending.set(key, utf8.encode(value));
+    pending.set(key, value);
   }
   return new prolly.Map(undefined, pending);
 }
 
 test('pending changes keys', async () => {
   const baseMap = new Map();
-  baseMap.set(b`a`, b`a`);
-  baseMap.set(b`b`, b`b`);
+  baseMap.set('a', 'a');
+  baseMap.set('b', 'b');
 
-  const base = Leaf.new(baseMap);
+  const base = new Leaf([...baseMap]);
   const map = new prolly.Map(base, new Map());
 
-  map.put(b`c`, b`c`);
+  map.put('c', 'c');
   expect(map.pendingChangedKeys()).to.deep.equal(['c']);
 
   // Set b to b again... should be a nop
-  map.put(b`b`, b`b`);
+  map.put('b', 'b');
   expect(map.pendingChangedKeys()).to.deep.equal(['c']);
 
   // Remove c from pending
-  map.del(b`c`);
+  map.del('c');
   expect(map.pendingChangedKeys()).to.deep.equal([]);
 
-  map.del(b`d`);
+  map.del('d');
   expect(map.pendingChangedKeys()).to.deep.equal([]);
 
-  map.put(b`b`, b`2`);
+  map.put('b', '2');
   expect(map.pendingChangedKeys()).to.deep.equal(['b']);
 });

src/asserts.ts
@@ -51,3 +51,12 @@ export function assertNotUndefined<T>(v: T | undefined): asserts v is T {
     throw new Error('Expected non undefined value');
   }
 }
+
+export function assertInstanceof<T>(
+  v: unknown,
+  t: new (...args: unknown[]) => T,
+): asserts v is T {
+  if (!(v instanceof t)) {
+    throw new Error(`Expected instanceof ${t.name}`);
+  }
+}

src/dag/chunk.ts
@@ -1,37 +1,33 @@
 import * as flatbuffers from 'flatbuffers';
 import {Hash} from '../hash';
+import type {Value} from '../kv/store';
 import {Meta} from './generated/meta/meta';
 
+// TODO(arv): Make this class take a type parameter for the data type?
 export class Chunk {
   readonly hash: string;
-  readonly data: Uint8Array;
+  readonly data: Value;
   /**
    * Meta is a Meta.fbs containing refs if there are any refs. If there are no
    * refs we do not write a meta chunk.
    */
   readonly meta: Uint8Array | undefined;
 
-  private constructor(
-    hash: string,
-    data: Uint8Array,
-    meta: Uint8Array | undefined,
-  ) {
+  private constructor(hash: string, data: Value, meta: Uint8Array | undefined) {
     this.hash = hash;
     this.data = data;
     this.meta = meta;
   }
 
-  static new(data: Uint8Array, refs: string[]): Chunk {
-    const hash = Hash.of(data);
+  static new(data: Value, refs: string[]): Chunk {
+    // Use hash of JSON stringified data if a JSONValue is passed.
+    const sum = ArrayBuffer.isView(data) ? data : JSON.stringify(data);
+    const hash = Hash.of(sum);
     const meta = createMeta(refs);
     return new Chunk(hash.toString(), data, meta);
   }
 
-  static read(
-    hash: string,
-    data: Uint8Array,
-    meta: Uint8Array | undefined,
-  ): Chunk {
+  static read(hash: string, data: Value, meta: Uint8Array | undefined): Chunk {
     return new Chunk(hash, data, meta);
   }
 }

src/dag/dag.ts
@@ -1,21 +1,0 @@
-export type HeadChange = {
-  new: string | undefined;
-  old: string | undefined;
-};
-
-export function toLittleEndian(count: number): Uint8Array {
-  if (count < 0 || count > 0xffff) {
-    throw new Error('Ref count out of range');
-  }
-  const buf = new Uint8Array(2);
-  buf[0] = count & 0xff;
-  buf[1] = (count >> 8) & 0xff;
-  return buf;
-}
-
-export function fromLittleEndian(buf: Uint8Array): number {
-  if (buf.length !== 2) {
-    throw new Error('Ref count must be 2 bytes');
-  }
-  return buf[0] | (buf[1] << 8);
-}

src/dag/read.ts
@@ -2,6 +2,7 @@ import type * as kv from '../kv/mod';
 import {Chunk} from './chunk';
 import {chunkDataKey, chunkMetaKey, headKey} from './key';
 import * as utf8 from '../utf8';
+import {assertInstanceof} from '../asserts';
 
 export class Read {
   private readonly _kvr: kv.Read;
@@ -21,12 +22,19 @@ export class Read {
     }
 
     const meta = await this._kvr.get(chunkMetaKey(hash));
+    if (meta !== undefined) {
+      assertInstanceof(meta, Uint8Array);
+    }
     return Chunk.read(hash, data, meta);
   }
 
   async getHead(name: string): Promise<string | undefined> {
     const data = await this._kvr.get(headKey(name));
-    return data && utf8.decode(data);
+    if (data === undefined) {
+      return undefined;
+    }
+    assertInstanceof(data, Uint8Array);
+    return utf8.decode(data);
   }
 
   close(): void {

src/dag/write.test.ts
@@ -4,17 +4,18 @@ import {Chunk} from './chunk';
 import {chunkDataKey, chunkMetaKey, chunkRefCountKey, headKey} from './key';
 import {Write} from './write';
 import type * as kv from '../kv/mod';
-import {fromLittleEndian} from './dag';
 import {Read} from './read';
 import * as utf8 from '../utf8';
 import {initHasher} from '../hash';
+import {assertInstanceof} from '../asserts';
+import type {Value} from '../kv/store';
 
 setup(async () => {
   await initHasher();
 });
 
 test('put chunk', async () => {
-  const t = async (data: Uint8Array, refs: string[]) => {
+  const t = async (data: Value, refs: string[]) => {
     const kv = new MemStore();
     await kv.withWrite(async kvw => {
       const w = new Write(kvw);
@@ -39,18 +40,28 @@ test('put chunk', async () => {
   await t(new Uint8Array([]), []);
   await t(new Uint8Array([0]), ['r1']);
   await t(new Uint8Array([0, 1]), ['r1', 'r2']);
+
+  await t(0, []);
+  await t(42, []);
+  await t(true, []);
+  await t(false, []);
+  await t('', []);
+  await t('hello', []);
+  await t([], []);
+  await t([1], []);
+  await t({}, []);
+  await t({a: 42}, []);
 });
 
 async function assertRefCount(kvr: kv.Read, hash: string, count: number) {
-  const buf = await kvr.get(chunkRefCountKey(hash));
+  const value = await kvr.get(chunkRefCountKey(hash));
   if (count === 0) {
-    expect(buf).to.be.undefined;
+    expect(value).to.be.undefined;
   } else {
-    if (buf === undefined) {
-      throw new Error('buf is undefined');
+    if (value === undefined) {
+      throw new Error('value is undefined');
     }
-    const metaCount = fromLittleEndian(buf);
-    expect(metaCount).to.equal(count);
+    expect(value).to.equal(count);
   }
 }
 
@@ -61,6 +72,7 @@ test('set head', async () => {
       await w.setHead(name, hash);
       if (hash !== undefined) {
         const h = await kvw.get(headKey(name));
+        assertInstanceof(h, Uint8Array);
         expect(hash).to.equal(h && utf8.decode(h));
       } else {
         expect(await kvw.get(headKey(name))).to.be.undefined;

commit 88374a6e786ed3c9ebac87de4ed317078324b068

Fix bug introduced in 48c699c27abf62565360a771d4fb935f7ab8f95a:
All our sample apps directly mutate the data returned by get()
(and less commonly scan()). This was breaking change detection
completely.

src/json.ts
@@ -92,6 +92,26 @@ export function deepEqual(
   return true;
 }
 
+export function deepFreeze<T extends JSONValue>(value: T): T {
+  if (Object.isFrozen(value)) {
+    return value;
+  }
+  if (Array.isArray(value)) {
+    Object.freeze(value);
+    for (const v of value) {
+      deepFreeze(v);
+    }
+  } else if (typeof value === 'object' && value !== null) {
+    Object.freeze(value);
+    for (const v of Object.values(value)) {
+      if (v !== undefined) {
+        deepFreeze(v);
+      }
+    }
+  }
+  return value;
+}
+
 export function assertJSONValue(v: unknown): asserts v is JSONValue {
   switch (typeof v) {
     case 'boolean':

src/kv/write-impl-base.ts
@@ -1,3 +1,4 @@
+import {deepFreeze} from '../json';
 import type {Read, Value} from './store';
 
 export const deleteSentinel = Symbol();
@@ -35,7 +36,7 @@ export class WriteImplBase {
   }
 
   async put(key: string, value: Value): Promise<void> {
-    this._pending.set(key, value);
+    this._pending.set(key, value instanceof Uint8Array ? value : deepFreeze(value));
   }
 
   async del(key: string): Promise<void> {

src/prolly/map.ts
@@ -1,9 +1,10 @@
 import type * as dag from '../dag/mod';
-import {deepEqual, JSONValue} from '../json';
+import {deepEqual, deepFreeze, JSONValue} from '../json';
 import {arrayCompare} from './array-compare';
 import {Leaf} from './leaf';
 import {PeekIterator} from './peek-iterator';
 import {stringCompare} from './string-compare';
+import cloneDeep from 'lodash-es/cloneDeep';
 
 export type Entry = [key: string, value: JSONValue];
 
@@ -60,9 +61,9 @@ class ProllyMap {
       case deleteSentinel:
         return undefined;
       case undefined:
-        return this._baseGet(key);
+        return cloneDeep(this._baseGet(key));
       default:
-        return p;
+        return cloneDeep(p);
     }
   }
 
@@ -78,7 +79,7 @@ class ProllyMap {
   }
 
   put(key: string, val: JSONValue): void {
-    this._pending.set(key, val);
+    this._pending.set(key, deepFreeze(val));
   }
 
   del(key: string): void {
@@ -214,7 +215,7 @@ class Iter implements IterableIterator<Entry> {
         // Key was deleted
         continue;
       }
-      return ni as IteratorResult<Entry>;
+      return cloneDeep(ni) as IteratorResult<Entry>;
     }
   }
 

src/json.test.ts
@@ -1,5 +1,5 @@
-import {assert} from '@esm-bundle/chai';
-import {deepEqual} from './json';
+import {assert, expect} from '@esm-bundle/chai';
+import {deepEqual, deepFreeze} from './json';
 import type {JSONValue} from './json';
 
 const {fail} = assert;
@@ -53,4 +53,15 @@ test('JSON deep equal', () => {
   }
 
   t({a: 1, b: 2}, {b: 2, a: 1});
+});
+
+test('JSON deepFreeze', async () => {
+  const obj: any = deepFreeze({obj:{}, arr:[]});
+  const arr: any = deepFreeze([{}, []]);
+  expect(() => obj.foo = 'bar').throws();
+  expect(() => obj.obj.foo = 'bar').throws();
+  expect(() => obj.arr[0] = 'bar').throws();
+  expect(() => arr[0] = 'bar').throws();
+  expect(() => arr[0].foo = 'bar').throws();
+  expect(() => arr[1][0] = 'bar').throws();
 });

src/prolly/map.test.ts
@@ -281,3 +281,40 @@ test('pending changes keys', async () => {
   map.put('b', '2');
   expect(map.pendingChangedKeys()).to.deep.equal(['b']);
 });
+
+test('caller mutates read data', async () => {
+  const t = ({readViaGet, putMutated}: {
+    readViaGet: boolean, // or scan
+    putMutated: boolean,
+  }) => {
+    const original = {foo: 'bar'};
+    const map = new prolly.Map(new Leaf([['foo', original]]), new Map());
+    const mutated = readViaGet ? map.get('foo') : map.entries().next().value[1];
+
+    // We expect to get back a different object each time right now (this may change in the future).
+    expect(mutated).is.not.equal(original);
+    expect(mutated).to.deep.equal(original);
+
+    // Mutate the returned object
+    mutated.foo = 'baz';
+
+    // Write the object
+    if (putMutated) {
+      map.put('foo', mutated);
+    }
+
+    const actual = map.get('foo');
+    if (putMutated) {
+      expect(map.pendingChangedKeys()).to.deep.equal(['foo']);
+      expect(actual).to.deep.equal(mutated);
+    } else {
+      expect(map.pendingChangedKeys()).to.deep.equal([]);
+      expect(actual).to.deep.equal(original);
+    }
+  };
+
+  t({readViaGet: true, putMutated: false});
+  t({readViaGet: false, putMutated: false});
+  t({readViaGet: true, putMutated: true});
+  t({readViaGet: false, putMutated: true});
+});
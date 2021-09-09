8840a482eaf3a302a6d6b2a5f56785b4832205e7
Erik Arvidsson <arv@roci.dev>
2021-09-08 21:25:46 -1000

    Restore Uint8array support for client id (#498)
    This adds code to support reading the client ID as Uint8Array. Since the
    client ID is only written once, we also upgrade the value to a string.
    
    Towards #488

--- src/sync/client-id.ts
+++ src/sync/client-id.ts
@@ -1,19 +1,32 @@
 import type * as kv from '../kv/mod';
 import {uuid as makeUuid} from './uuid';
 import {assertString} from '../asserts';
+import {READ_FLATBUFFERS} from '../dag/config';
+import * as utf8 from '../utf8';
 
-const CID_KEY = 'sys/cid';
+export const CID_KEY = 'sys/cid';
 
-export async function init(s: kv.Store): Promise<string> {
-  const cid = await s.withRead(r => r.get(CID_KEY));
+export async function init(store: kv.Store): Promise<string> {
+  const cid = await store.withRead(r => r.get(CID_KEY));
   if (cid !== undefined) {
+    if (READ_FLATBUFFERS) {
+      if (cid instanceof Uint8Array) {
+        const str = utf8.decode(cid);
+        await writeClientID(store, str);
+        return str;
+      }
+    }
     assertString(cid);
     return cid;
   }
   const uuid = makeUuid();
-  await s.withWrite(async wt => {
+  await writeClientID(store, uuid);
+  return uuid;
+}
+
+function writeClientID(s: kv.Store, uuid: string): Promise<void> {
+  return s.withWrite(async wt => {
     await wt.put(CID_KEY, uuid);
     await wt.commit();
   });
-  return uuid;
 }

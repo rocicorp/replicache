266954d27656d1c8dcd622e58455b53ff831b4a3
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-10-04 16:29:05 -0700

    Add JSON hash function perf test

--- perf/hash.ts
+++ perf/hash.ts
@@ -1,5 +1,7 @@
 import {createSHA512} from 'hash-wasm';
-import {makeRandomStrings} from './data';
+import {hashJSON} from '../src/hash-json';
+import type {ReadonlyJSONValue} from '../src/json';
+import {randomString, makeRandomStrings} from './data';
 import type {Benchmark} from './perf';
 
 const encoder = new TextEncoder();
@@ -18,12 +20,16 @@ function stringToUint16Array(s: string): Uint16Array {
 
 export function benchmarks(): Array<Benchmark> {
   return [
-    textEncoderUtf8(),
-    textEncoderUtf16(),
-    sha512({wasm: true, utf8: true}),
-    sha512({wasm: true, utf8: false}),
-    sha512({wasm: false, utf8: true}),
-    sha512({wasm: false, utf8: false}),
+    // textEncoderUtf8(),
+    // textEncoderUtf16(),
+    // sha512({wasm: true, utf8: true}),
+    // sha512({wasm: true, utf8: false}),
+    // sha512({wasm: false, utf8: true}),
+    // sha512({wasm: false, utf8: false}),
+    hashJSONBenchmark({useHashJSON: true, utf8: false}),
+    hashJSONBenchmark({useHashJSON: true, utf8: true}),
+    hashJSONBenchmark({useHashJSON: false, utf8: true}),
+    hashJSONBenchmark({useHashJSON: false, utf8: false}),
   ];
 }
 
@@ -92,6 +98,68 @@ function sha512({wasm, utf8}: {wasm: boolean; utf8: boolean}): Benchmark {
         const buf = await calculateHash(sum);
         results.push(buf);
       }
+    },
+  };
+}
+
+const testObject1 = [
+  true,
+  false,
+  null,
+  0,
+  1.23,
+  42,
+  256,
+  -127,
+  -1,
+  -1e50,
+  makeRandomStrings(10, 10000),
+  Object.fromEntries(
+    Array.from({length: 10}, () => [randomString(10), randomString(10000)]),
+  ),
+];
+
+const testObject = Array.from({length: 10}, () => testObject1);
+
+function hashJSONBenchmark({
+  useHashJSON,
+  utf8,
+}: {
+  useHashJSON: boolean;
+  utf8?: boolean;
+}): Benchmark {
+  const results = [];
+  let calculateHash: (
+    value: ReadonlyJSONValue,
+  ) => Uint8Array | Promise<ArrayBuffer>;
+  const toSum = utf8 ? stringToUint8Array : stringToUint16Array;
+
+  return {
+    name: `sha512 ${useHashJSON ? 'hashJSON' : 'JSON.stringify'} from string ${
+      utf8 ? 'utf8' : 'utf16'
+    }`,
+    group: 'hash',
+    async setup() {
+      const hasher = await createSHA512();
+      if (useHashJSON) {
+        calculateHash = value => {
+          hasher.init();
+          hashJSON(value, hasher, toSum);
+          return hasher.digest('binary');
+        };
+      } else {
+        calculateHash = value => {
+          hasher.init();
+          hasher.update(toSum(JSON.stringify(value)));
+          return hasher.digest('binary');
+        };
+      }
+    },
+    async run() {
+      // for (let i = 0; i < 10; i++) {
+      const buf = await calculateHash(testObject);
+      results.push(buf);
+      // }
     },
   };
 }

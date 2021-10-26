e843bb7e590f6b05e327c9714a861138d5905b9f
Aaron Boodman <aaron@aaronboodman.com>
2021-10-25 17:10:17 -1000

    Update indexes incrementally after pull.
    Fixes #601

---
+++ src/lazy.ts
@@ -0,0 +1,9 @@
+export function lazy<T>(factory: () => T): () => T {
+  let value: T | undefined;
+  return () => {
+    if (value === undefined) {
+      value = factory();
+    }
+    return value;
+  };
+}

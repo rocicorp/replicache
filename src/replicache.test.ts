d53ce3ca522f9a7254a5540381009e810b78448e
Erik Arvidsson <erik.arvidsson@gmail.com>
2021-11-01 14:48:42 -0700

    Last minute changes

--- src/replicache.test.ts
+++ src/replicache.test.ts
@@ -2785,7 +2785,7 @@ test('experiment KV Store', async () => {
     mutators: {addData},
   });
 
-  expect(store.readCount).to.equal(5);
+  expect(store.readCount).to.equal(4);
   expect(store.writeCount).to.equal(5);
   expect(store.closeCount).to.equal(0);
   store.resetCounters();
@@ -2817,7 +2817,7 @@ test('subscription coalescing', async () => {
     mutators: {addData},
   });
 
-  expect(store.readCount).to.equal(5);
+  expect(store.readCount).to.equal(4);
   expect(store.writeCount).to.equal(5);
   expect(store.closeCount).to.equal(0);
   store.resetCounters();

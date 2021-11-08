4918cf544557065c0288a5bf3f44bba11bb97806
Gregory Baker <greg@roci.dev>
2021-11-08 15:38:10 -0700

    work

---
+++ src/worker-tests/worker-test.ts
@@ -0,0 +1,74 @@
+// This test file is loaded by worker.test.ts
+
+import {ReplicacheTest} from '../test-util';
+import type {ReadTransaction, WriteTransaction} from '../transactions';
+import {asyncIterableToArray} from '../async-iterable-to-array';
+import {expect} from '@esm-bundle/chai';
+import type {JSONValue} from '../json';
+import {closeAllReps, reps} from '../test-util';
+
+onmessage = async (e: MessageEvent) => {
+  const {name, useMemstore} = e.data;
+  try {
+    await testGethasScanOnEmptyDB(name, useMemstore);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore TypeScripts type defs are incorrect.
+    postMessage(undefined);
+    await closeAllReps();
+  } catch (ex) {
+    await closeAllReps();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore TypeScripts type defs are incorrect.
+    postMessage(ex);
+  }
+};
+
+async function testGethasScanOnEmptyDB(name: string, useMemstore = false) {
+  const rep = new ReplicacheTest({
+    pushDelay: 60_000, // Large to prevent interferin;,
+    name,
+    useMemstore,
+    mutators: {
+      testMut: async (
+        tx: WriteTransaction,
+        args: {key: string; value: JSONValue},
+      ) => {
+        const {key, value} = args;
+        await tx.put(key, value);
+        expect(await tx.has(key)).to.equal(true);
+        const v = await tx.get(key);
+        expect(v).to.deep.equal(value);
+
+        expect(await tx.del(key)).to.equal(true);
+        expect(await tx.has(key)).to.be.false;
+      },
+    },
+  });
+  reps.add(rep);
+
+  const {testMut} = rep.mutate;
+
+  for (const [key, value] of Object.entries({
+    a: true,
+    b: false,
+    c: null,
+    d: 'string',
+    e: 12,
+    f: {},
+    g: [],
+    h: {h1: true},
+    i: [0, 1],
+  })) {
+    await testMut({key, value: value as JSONValue});
+  }
+
+  async function t(tx: ReadTransaction) {
+    expect(await tx.get('key')).to.equal(undefined);
+    expect(await tx.has('key')).to.be.false;
+
+    const scanItems = await asyncIterableToArray(tx.scan());
+    expect(scanItems).to.have.length(0);
+  }
+
+  await rep.query(t);
+}
---
+++ src/worker-tests/worker.test.ts
@@ -0,0 +1,54 @@
+import {expect} from '@esm-bundle/chai';
+import {sleep} from '../sleep';
+import {closeAllReps, dbsToDrop, deletaAllDatabases} from '../test-util';
+
+teardown(async () => {
+  await closeAllReps();
+  deletaAllDatabases();
+});
+
+// This started failing on github only with https://github.com/rocicorp/replicache/pull/479.
+// It works fine locally. Error message is:
+// src/worker.test.ts:
+//
+// ❌ worker test (failed on Chromium)
+// Error: Timed out
+//   at src/worker.test.ts:42:42
+//   at async o.<anonymous> (src/worker.test.ts:17:17)
+//
+// Example failure: https://github.com/rocicorp/replicache/runs/3519251318
+test.skip('worker test', async () => {
+  const url = new URL('./worker-test.ts', import.meta.url);
+  const w = new Worker(url, {type: 'module'});
+  const name = 'worker-test';
+  dbsToDrop.add(name);
+
+  {
+    const data = await send(w, {name, useMemstore: false});
+    expect(data).to.be.undefined;
+  }
+  {
+    const data = await send(w, {name, useMemstore: true});
+    expect(data).to.be.undefined;
+  }
+});
+
+async function send(
+  w: Worker,
+  data: {name: string; useMemstore: boolean},
+): Promise<unknown> {
+  const p = new Promise((resolve, reject) => {
+    w.onmessage = e => resolve(e.data);
+    w.onerror = reject;
+    w.onmessageerror = reject;
+  });
+  w.postMessage(data);
+  return withTimeout(p);
+}
+
+async function withTimeout<T>(p: Promise<T>): Promise<T> {
+  return Promise.race([
+    p,
+    sleep(3000).then(() => Promise.reject(new Error('Timed out'))),
+  ]);
+}

ea14f5ad2f1fc16097abd28f53ad6d9311487436
github-action-benchmark <github@users.noreply.github.com>
2021-11-09 16:10:32 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1fa2cfdc6334cbc8db8c506477397c4590b44e44

--- bundle-sizes/data.js
+++ bundle-sizes/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636427767535,
+  "lastUpdate": 1636474232472,
   "repoUrl": "https://github.com/rocicorp/replicache",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/rocicorp/replicache/commit/4813b8209c788bd8f1819bcf0de161a5881fe423"
         },
         "date": 1636427767273,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "replicache.js size",
+            "value": 184990,
+            "unit": "bytes"
+          },
+          {
+            "name": "replicache.mjs size",
+            "value": 184636,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "greg@roci.dev",
+            "name": "Greg Baker",
+            "username": "grgbkr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1fa2cfdc6334cbc8db8c506477397c4590b44e44",
+          "message": "refactor: Rework test config to avoid missing new tests in src/ (#676)\n\n### Problem\r\nNew tests added to the `src/` dir are not run by `npm run test` unless the are explicitly added to `web-test-runner.config.mjs`.  This is unexpected as tests added to subdirs of src are automatically included, and so I was confused for sometime as to why a new test I added wasn't running.  \r\n\r\nThe reason for this is we don't want to run `src/worker.test.ts` with the other tests.\r\n\r\n### Solution\r\nMove `worker.test.ts` and `worker-test.ts` to a subdir `worker-tests`, and add `src/*.test.ts` to files config in `web-test-runner.config.mjs`.  Now new tests added to `src/` will automatically be included in `npm run test`.",
+          "timestamp": "2021-11-09T16:10:00Z",
+          "tree_id": "f9ca8796a9b450624c4b8841eccf4309cc1012ab",
+          "url": "https://github.com/rocicorp/replicache/commit/1fa2cfdc6334cbc8db8c506477397c4590b44e44"
+        },
+        "date": 1636474232159,
         "tool": "customSmallerIsBetter",
         "benches": [
           {

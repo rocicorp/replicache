4813b8209c788bd8f1819bcf0de161a5881fe423
Greg Baker <greg@roci.dev>
2021-11-08 19:15:39 -0800

    feat: Setup dashboard and alerts for bundle sizes (#677)
    ### Problem
    We are missing a way to see bundle size changes over time (i.e. across commits).  It would also be nice to get alerts when bundle sizes increase by more than a small percent.
    
    ### Solution
    
    - Create a small node script that outputs the bundle sizes in the format needed for the github-action-benchmark customSmallerIsBetter tool.  
    - Set up yml for github action that will run this script and use github-action-benchmark to graph the sizes on a new dashboard at https://rocicorp.github.io/replicache/bundle-sizes/
    - Also configure an alert for when bundle sizes increase by more than **5%.** 
    
    Closes #125

---
+++ perf/bundle-sizes.js
@@ -0,0 +1,55 @@
+/* eslint-env node, es2020 */
+
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+import * as path from 'path';
+import {promises as fs} from 'fs';
+
+async function main() {
+  const optionDefinitions = [
+    {
+      name: 'bundles',
+      multiple: true,
+      defaultValue: ['replicache.js', 'replicache.mjs'],
+      description: 'bundle files whose sizes should be included in output',
+    },
+    {
+      name: 'dir',
+      defaultValue: './out',
+      description: 'directory where bundles are located',
+    },
+    {
+      name: 'help',
+      alias: 'h',
+      type: Boolean,
+      description: 'Show this help message',
+    },
+  ];
+  const options = commandLineArgs(optionDefinitions);
+  if (options.help) {
+    console.log(
+      commandLineUsage([
+        {content: 'Usage: module-sizes [options...]'},
+        {optionList: optionDefinitions},
+      ]),
+    );
+    process.exit();
+  }
+
+  const jsonEntries = [];
+  for (const bundle of options.bundles) {
+    const stats = await fs.stat(path.join(options.dir, bundle));
+    jsonEntries.push({
+      name: `${bundle} size`,
+      unit: 'bytes',
+      value: stats.size,
+    });
+  }
+
+  process.stdout.write(JSON.stringify(jsonEntries, undefined, 2) + '\n');
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});

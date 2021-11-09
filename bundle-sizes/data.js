495d498d4617cccf9b1585a3dc7a3190833e2094
github-action-benchmark <github@users.noreply.github.com>
2021-11-09 17:00:23 +0000

    add Bundle Sizes (customSmallerIsBetter) benchmark result for da05aa3319dedf3ad93862c2349cc097e41d9382

--- bundle-sizes/data.js
+++ bundle-sizes/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636476231136,
+  "lastUpdate": 1636477223960,
   "repoUrl": "https://github.com/rocicorp/replicache",
   "entries": {
     "Benchmark": [
@@ -171,6 +171,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "replicache.mjs size",
             "value": 184636,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ],
+    "Bundle Sizes": [
+      {
+        "commit": {
+          "author": {
+            "email": "greg@roci.dev",
+            "name": "Greg Baker",
+            "username": "grgbkr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "da05aa3319dedf3ad93862c2349cc097e41d9382",
+          "message": "feat: Add Brotli compressed bundle sizes to Bundle Sizes dashboard (#679)\n\n### Problem\r\nBundle Size dashboard https://rocicorp.github.io/replicache/bundle-sizes/ and associated alerts currently only track non-compressed sizes of bundles.  What we really care about is Brotli compressed size.\r\n\r\n### Solution\r\nAdd Brotli compressed sizes of bundles to dashboard and alert.\r\nTo do this needed to move from `self-hosted` runner to `ubuntu-latest` as `brotli` command was not available in the `self-hosted` environment (but is in `ubuntu-latest`).  This is fine as we don't care about cpu/memory isolation for this benchmark as we do for the performance benchmarks, because we are just measuring byte size.",
+          "timestamp": "2021-11-09T08:59:18-08:00",
+          "tree_id": "e6c7667d804131fff367901c076321d5c4d8751a",
+          "url": "https://github.com/rocicorp/replicache/commit/da05aa3319dedf3ad93862c2349cc097e41d9382"
+        },
+        "date": 1636477222293,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Size of replicache.js",
+            "value": 184990,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.js.br (Brotli compressed)",
+            "value": 34800,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs",
+            "value": 184636,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs.br (Brotli compressed)",
+            "value": 34659,
             "unit": "bytes"
           }
         ]

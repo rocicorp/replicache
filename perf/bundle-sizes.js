da05aa3319dedf3ad93862c2349cc097e41d9382
Greg Baker <greg@roci.dev>
2021-11-09 08:59:18 -0800

    feat: Add Brotli compressed bundle sizes to Bundle Sizes dashboard (#679)
    ### Problem
    Bundle Size dashboard https://rocicorp.github.io/replicache/bundle-sizes/ and associated alerts currently only track non-compressed sizes of bundles.  What we really care about is Brotli compressed size.
    
    ### Solution
    Add Brotli compressed sizes of bundles to dashboard and alert.
    To do this needed to move from `self-hosted` runner to `ubuntu-latest` as `brotli` command was not available in the `self-hosted` environment (but is in `ubuntu-latest`).  This is fine as we don't care about cpu/memory isolation for this benchmark as we do for the performance benchmarks, because we are just measuring byte size.

--- perf/bundle-sizes.js
+++ perf/bundle-sizes.js
@@ -40,7 +40,9 @@ async function main() {
   for (const bundle of options.bundles) {
     const stats = await fs.stat(path.join(options.dir, bundle));
     jsonEntries.push({
-      name: `${bundle} size`,
+      name: `Size of ${bundle}${
+        bundle.endsWith('.br') ? ' (Brotli compressed)' : ''
+      }`,
       unit: 'bytes',
       value: stats.size,
     });

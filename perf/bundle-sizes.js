4355429a168754ee6ed2abc97b10d84a1adacfa5
Gregory Baker <greg@roci.dev>
2021-11-09 09:53:02 -0700

    beautify a bit

--- perf/bundle-sizes.js
+++ perf/bundle-sizes.js
@@ -40,7 +40,7 @@ async function main() {
   for (const bundle of options.bundles) {
     const stats = await fs.stat(path.join(options.dir, bundle));
     jsonEntries.push({
-      name: `Size of ${bundle}`,
+      name: `Size of ${bundle}${bundle.endsWith('.br') ? ' (Brotli compressed)' :''}`,
       unit: 'bytes',
       value: stats.size,
     });

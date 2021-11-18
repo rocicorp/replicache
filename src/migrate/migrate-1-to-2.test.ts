8c50cb4dce1267fdf89c9bf67ce2e3b00df128b0
Erik Arvidsson <arv@roci.dev>
2021-11-18 13:37:11 -0800

    fix: Fix migraion of head (#708)
    The migration from v1 to v2 was not updating the head so the migrated
    dag was GC'ed and the old dag was being kept.
    
    Fixes #704

---
+++ src/migrate/migrate-1-to-2.test.ts
@@ -0,0 +1,52 @@
+import {expect} from '@esm-bundle/chai';
+import {migrateProllyMap} from './migrate-1-to-2';
+import * as dag from '../dag/mod';
+import * as kv from '../kv/mod';
+import * as prolly from '../prolly/mod';
+import {emptyHash, initHasher} from '../hash';
+
+setup(async () => {
+  await initHasher();
+});
+
+test('migrateProllyMap', async () => {
+  const t = async (entries: prolly.Entry[]) => {
+    const kvStore = new kv.TestMemStore();
+    const dagStore = new dag.TestStore(kvStore);
+
+    const oldHash = await dagStore.withWrite(async dagWrite => {
+      const map = new prolly.Map(entries);
+      const h = await map.flush(dagWrite);
+      await dagWrite.setHead('testOld', h);
+      await dagWrite.commit();
+      return h;
+    });
+
+    const newHash = await dagStore.withWrite(async dagWrite => {
+      const newHash = await migrateProllyMap(oldHash, dagWrite, new Map());
+      await dagWrite.setHead('testNew', newHash);
+      await dagWrite.commit();
+      return newHash;
+    });
+
+    if (entries.length === 0) {
+      expect(newHash).to.equal(emptyHash);
+    } else {
+      const chunkData = kvStore.map().get(dag.chunkDataKey(newHash));
+      expect(chunkData).to.deep.equal([0, entries]);
+    }
+  };
+
+  await t([]);
+  await t([['foo', 42]]);
+  await t([['foo', false]]);
+  await t([['foo', 0]]);
+  await t([
+    ['bar', []],
+    ['foo', true],
+  ]);
+  await t([
+    ['x', {}],
+    ['y', {z: null}],
+  ]);
+});

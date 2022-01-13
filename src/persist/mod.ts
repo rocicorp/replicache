b770a25b38b5660978475c1c6180238c8f3b30fa
Greg Baker <greg@roci.dev>
2022-01-13 08:46:23 -0800

    refactor: Delete unused slurp and merge kv.MemStore into kv.TestMemStore (#778)
    With the integration of dag.LazyStore slurp is no longer used, and kv.MemStore is only 
    used via kv.TestMemStore in tests.

--- src/persist/mod.ts
+++ src/persist/mod.ts
@@ -2,4 +2,3 @@ export {persist} from './persist';
 export {startHeartbeats} from './heartbeat';
 export {initClient} from './clients';
 export {initClientGC} from './client-gc';
-export {slurp} from './slurp';

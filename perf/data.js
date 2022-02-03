dd22738ad9769fb1f1d2b6bfd60dc41f59b3b49f
github-action-benchmark <github@users.noreply.github.com>
2022-02-03 19:34:13 +0000

    add Benchmark (benchmarkjs) benchmark result for eab7fb6ffa9276450dd185cbbd43adc0f05b7c83

--- perf/data.js
+++ perf/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643782851890,
+  "lastUpdate": 1643916853870,
   "repoUrl": "https://github.com/rocicorp/replicache",
   "entries": {
     "Benchmark": [
@@ -67105,6 +67105,58 @@ window.BENCHMARK_DATA = {
             "name": "startup scan 1024x100 from 1024x100000 stored",
             "value": 1.85,
             "range": "±4.2%",
+            "unit": "MB/s",
+            "extra": "8 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arv@roci.dev",
+            "name": "Erik Arvidsson",
+            "username": "arv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eab7fb6ffa9276450dd185cbbd43adc0f05b7c83",
+          "message": "chore: Use esbuild mangle-props (#805)\n\nFor even smaller minimized output",
+          "timestamp": "2022-02-03T19:32:01Z",
+          "tree_id": "aa42e322ed574d072fc61e3c228a151ae56d4953",
+          "url": "https://github.com/rocicorp/replicache/commit/eab7fb6ffa9276450dd185cbbd43adc0f05b7c83"
+        },
+        "date": 1643916853352,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "writeSubRead 1MB total, 64 subs total, 5 subs dirty, 16kb read per sub",
+            "value": 769.23,
+            "range": "±2.5%",
+            "unit": "ops/sec",
+            "extra": "19 samples"
+          },
+          {
+            "name": "scan 1024x1000",
+            "value": 336.75,
+            "range": "±4.7%",
+            "unit": "MB/s",
+            "extra": "19 samples"
+          },
+          {
+            "name": "startup read 1024x100 from 1024x100000 stored",
+            "value": 0.66,
+            "range": "±181.6%",
+            "unit": "MB/s",
+            "extra": "7 samples"
+          },
+          {
+            "name": "startup scan 1024x100 from 1024x100000 stored",
+            "value": 1.78,
+            "range": "±5.9%",
             "unit": "MB/s",
             "extra": "8 samples"
           }

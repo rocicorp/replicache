rocicorp/replicache

commit 1538c424e2369c36c5a41c7876f0dad338e59768

feat: Simplified Dueling Dag - Mutation Recovery - Implement the mutation recovery process. (#799)
### Problem 
With Simplified Dueling Dags mutations that have not been synced to the server when a tab is 
unloaded (or frozen and never unfrozen) are lost.  This can occur in common user flows, and 
will result in unexpected data loss.  The impact is worst when the user has been offline or has 
a flakey connection as there will be more local mutations that have not been synced.   Cases 
where this will occur:

- Refresh before changes have been pushed
- Close before changes have been pushed
- Navigate away before changes have been pushed
- Tab backgrounded and frozen before changes have been pushed (seems unlikely) and tab is not revisited before client is gc’d
- Tab crash before changes have been pushed

### Solution
Replicache clients will try to recover mutations from other Replicache client's perdag state.   
A Replicache client can recover another Replicache client's mutations if the other clients has 
the same name (and thus can share auth), the same domain, and a Replicache format and 
schema version understood by the client.   A Replicache client will try to recover other
clients' mutation at startup, reconnection and on a 5 minute interval

See full design at https://www.notion.so/replicache/Mutation-Recovery-Avoiding-Mutation-Loss-using-PerDag-state-f54025b52cbc435692abca3307947d15

src/replicache.ts
@@ -43,6 +43,7 @@ import {Lock} from './rw-lock';
 import * as persist from './persist/mod';
 import {requestIdle} from './request-idle';
 import type {HTTPRequestInfo} from './http-request-info';
+import {assertNotUndefined} from './asserts';
 
 export type BeginPullResult = {
   requestID: string;
@@ -57,8 +58,10 @@ export type Poke = {
 
 export const httpStatusUnauthorized = 401;
 
-const REPLICACHE_FORMAT_VERSION = 3;
+export const REPLICACHE_FORMAT_VERSION = 3;
 const LAZY_STORE_SOURCE_CHUNK_CACHE_SIZE_LIMIT = 100 * 2 ** 20; // 100 MB
+const MUTATION_RECOVERY_LAZY_STORE_SOURCE_CHUNK_CACHE_SIZE_LIMIT = 10 * 2 ** 20; // 10 MB
+const RECOVER_MUTATIONS_INTERVAL_MS = 5 * 60 * 1000; // 5 mins
 
 export type MaybePromise<T> = T | Promise<T>;
 
@@ -216,9 +219,11 @@ export class Replicache<MD extends MutatorDefs = {}> {
 
   private _endHearbeats = noop;
   private _endClientsGC = noop;
+  private _recoverMutationsIntervalID: number | undefined = undefined;
 
   private readonly _persistLock = new Lock();
   private _persistIsScheduled = false;
+  private _recoveringMutations = false;
 
   /**
    * The options used to control the [[pull]] and push request behavior. This
@@ -367,6 +372,11 @@ export class Replicache<MD extends MutatorDefs = {}> {
 
     this._endHearbeats = persist.startHeartbeats(clientID, this._perdag);
     this._endClientsGC = persist.initClientGC(clientID, this._perdag);
+    this._recoverMutationsIntervalID = window.setInterval(
+      () => this._recoverMutations(),
+      RECOVER_MUTATIONS_INTERVAL_MS,
+    );
+    void this._recoverMutations();
   }
 
   /**
@@ -415,6 +425,7 @@ export class Replicache<MD extends MutatorDefs = {}> {
 
     this._endHearbeats();
     this._endClientsGC();
+    window.clearInterval(this._recoverMutationsIntervalID);
 
     await this._ready;
     const closingPromises = [
@@ -673,6 +684,9 @@ export class Replicache<MD extends MutatorDefs = {}> {
       if (this._online !== online) {
         this._online = online;
         this.onOnlineChange?.(online);
+        if (online) {
+          void this._recoverMutations();
+        }
       }
     }
   }
@@ -1149,6 +1163,218 @@ export class Replicache<MD extends MutatorDefs = {}> {
 
       return {result, ref};
     });
+  }
+
+  protected async _recoverMutations(): Promise<boolean> {
+    if (this._recoveringMutations || !this.online || this.closed) {
+      return false;
+    }
+    try {
+      this._recoveringMutations = true;
+      await this._ready;
+      await this._recoverMutationsFromPerdag(this._perdag, this.schemaVersion);
+      for (const database of Object.values(
+        await this._idbDatabases.getDatabases(),
+      )) {
+        if (database.name === this.idbName) {
+          continue;
+        }
+        if (database.replicacheFormatVersion !== REPLICACHE_FORMAT_VERSION) {
+          // TODO: when REPLICACHE_FORMAT_VERSION is updated
+          // need to also handle previous REPLICACHE_FORMAT_VERSIONs
+          continue;
+        }
+        const perKvStore = new IDBStore(database.name);
+        const perdag = new dag.StoreImpl(
+          perKvStore,
+          dag.throwChunkHasher,
+          assertNotTempHash,
+        );
+        await this._recoverMutationsFromPerdag(perdag, database.schemaVersion);
+      }
+    } catch (e) {
+      if (this.closed) {
+        this._lc.debug?.('Mutation recovery did not complete due to close.', e);
+      } else {
+        throw e;
+      }
+    } finally {
+      this._recoveringMutations = false;
+    }
+    return true;
+  }
+
+  private async _recoverMutationsFromPerdag(
+    perdag: dag.Store,
+    schemaVersion: string,
+  ): Promise<void> {
+    let clientMap: persist.ClientMap | undefined = await perdag.withRead(read =>
+      persist.getClients(read),
+    );
+    const clientIDsVisited = new Set<sync.ClientID>();
+    while (clientMap) {
+      let newClientMap: persist.ClientMap | undefined;
+      for (const [clientID, client] of clientMap) {
+        if (!clientIDsVisited.has(clientID)) {
+          clientIDsVisited.add(clientID);
+          newClientMap = await this._recoverMutationsOfClient(
+            client,
+            clientID,
+            perdag,
+            schemaVersion,
+          );
+          if (newClientMap) {
+            break;
+          }
+        }
+      }
+      clientMap = newClientMap;
+    }
+  }
+
+  /**
+   * @returns When mutations are recovered the resulting updated client map.
+   *   Otherwise undefined, which can be because there were no mutations to
+   *   recover, or because an error occurred when trying to recover the
+   *   mutations.
+   */
+  private async _recoverMutationsOfClient(
+    client: persist.Client,
+    clientID: sync.ClientID,
+    perdag: dag.Store,
+    schemaVersion: string,
+  ): Promise<persist.ClientMap | undefined> {
+    const selfClientID = await this._clientIDPromise;
+    if (selfClientID === clientID) {
+      return undefined;
+    }
+    if (client.lastServerAckdMutationID >= client.mutationID) {
+      return undefined;
+    }
+    const dagForOtherClient = new dag.LazyStore(
+      perdag,
+      MUTATION_RECOVERY_LAZY_STORE_SOURCE_CHUNK_CACHE_SIZE_LIMIT,
+      dag.throwChunkHasher,
+      assertHash,
+    );
+    try {
+      await dagForOtherClient.withWrite(async write => {
+        await write.setHead(db.DEFAULT_HEAD_NAME, client.headHash);
+        await write.commit();
+      });
+
+      const pushRequestID = sync.newRequestID(clientID);
+      const pushDescription = 'recoveringMutationsPush';
+      const pushLC = this._lc
+        .addContext('rpc', pushDescription)
+        .addContext('request_id', pushRequestID);
+      const pushSucceeded = await this._wrapInOnlineCheck(async () => {
+        const {result: pushResponse} = await this._wrapInReauthRetries(
+          async () => {
+            const pushResponse = await sync.push(
+              pushRequestID,
+              dagForOtherClient,
+              pushLC,
+              clientID,
+              this.pusher,
+              this.pushURL,
+              this.auth,
+              schemaVersion,
+            );
+            return {result: pushResponse, httpRequestInfo: pushResponse};
+          },
+          pushDescription,
+          this.pushURL,
+        );
+        return !!pushResponse && pushResponse.httpStatusCode === 200;
+      }, pushDescription);
+      if (!pushSucceeded) {
+        this._lc.debug?.(
+          `Client ${selfClientID} failed to recover mutations for client ` +
+            `${clientID} due to a push error.`,
+        );
+        return undefined;
+      }
+
+      const requestID = sync.newRequestID(clientID);
+      const pullDescription = 'recoveringMutationsPull';
+      const pullLC = this._lc
+        .addContext('rpc', pullDescription)
+        .addContext('request_id', requestID);
+      const beginPullRequest = {
+        pullAuth: this.auth,
+        pullURL: this.pullURL,
+        schemaVersion,
+        puller: this.puller,
+      };
+      let pullResponse: PullResponse | undefined;
+      const pullSucceeded = await this._wrapInOnlineCheck(async () => {
+        const {result: beginPullResponse} = await this._wrapInReauthRetries(
+          async () => {
+            const beginPullResponse = await sync.beginPull(
+              clientID,
+              beginPullRequest,
+              beginPullRequest.puller,
+              requestID,
+              dagForOtherClient,
+              pullLC,
+              false,
+            );
+            return {
+              result: beginPullResponse,
+              httpRequestInfo: beginPullResponse.httpRequestInfo,
+            };
+          },
+          pullDescription,
+          this.pullURL,
+        );
+        pullResponse = beginPullResponse.pullResponse;
+        return (
+          !!pullResponse &&
+          beginPullResponse.httpRequestInfo.httpStatusCode === 200
+        );
+      }, pullDescription);
+      if (!pullSucceeded) {
+        this._lc.debug?.(
+          `Client ${selfClientID} failed to recover mutations for client ` +
+            `${clientID} due to a pull error.`,
+        );
+        return undefined;
+      }
+
+      this._lc.debug?.(
+        `Client ${selfClientID} recovered mutations for client ` +
+          `${clientID}.  Details`,
+        {
+          mutationID: client.mutationID,
+          lastServerAckdMutationID: client.lastServerAckdMutationID,
+          lastMutationID: pullResponse?.lastMutationID,
+        },
+      );
+      const newClientMap = await persist.updateClients(
+        (clients: persist.ClientMap) => {
+          assertNotUndefined(pullResponse);
+          const clientToUpdate = clients.get(clientID);
+          if (
+            !clientToUpdate ||
+            clientToUpdate.lastServerAckdMutationID >=
+              pullResponse.lastMutationID
+          ) {
+            return persist.noClientUpdates;
+          }
+          return {
+            clients: new Map(clients).set(clientID, {
+              ...clientToUpdate,
+              lastServerAckdMutationID: pullResponse.lastMutationID,
+            }),
+          };
+        },
+        perdag,
+      );
+      return newClientMap;
+    } finally {
+      await dagForOtherClient.close();
+    }
   }
 }
 

src/persist/clients.ts
@@ -2,15 +2,15 @@ import {assertHash, Hash, hashOf} from '../hash';
 import * as btree from '../btree/mod';
 import * as dag from '../dag/mod';
 import * as db from '../db/mod';
+import type * as sync from '../sync/mod';
 import type {ReadonlyJSONValue} from '../json';
 import {assertNotUndefined, assertNumber, assertObject} from '../asserts';
 import {hasOwn} from '../has-own';
-import type {ClientID} from '../sync/client-id';
 import {uuid as makeUuid} from '../sync/uuid';
 import {getRefs, newSnapshotCommitData} from '../db/commit';
 import type {MaybePromise} from '../mod';
 
-export type ClientMap = ReadonlyMap<ClientID, Client>;
+export type ClientMap = ReadonlyMap<sync.ClientID, Client>;
 
 export type Client = {
   /**
@@ -113,7 +113,7 @@ async function getClientsAtHash(
 }
 
 export async function getClient(
-  id: ClientID,
+  id: sync.ClientID,
   dagRead: dag.Read,
 ): Promise<Client | undefined> {
   const clients = await getClients(dagRead);
@@ -122,7 +122,7 @@ export async function getClient(
 
 export async function initClient(
   dagStore: dag.Store,
-): Promise<[ClientID, Client]> {
+): Promise<[sync.ClientID, Client]> {
   const newClientID = makeUuid();
   const updatedClients = await updateClients(async clients => {
     let bootstrapClient: Client | undefined;

src/persist/idb-databases-store.test.ts
@@ -60,3 +60,36 @@ test('close closes kv store', async () => {
   await store.close();
   expect(memstore.closed).to.be.true;
 });
+
+test('clear', async () => {
+  const store = new IDBDatabasesStore(_ => new TestMemStore());
+  const testDB1 = {
+    name: 'testName1',
+    replicacheFormatVersion: 1,
+    schemaVersion: 'testSchemaVersion1',
+  };
+
+  expect(await store.putDatabase(testDB1)).to.deep.equal({
+    testName1: testDB1,
+  });
+  expect(await store.getDatabases()).to.deep.equal({
+    testName1: testDB1,
+  });
+
+  await store.clear();
+
+  expect(await store.getDatabases()).to.deep.equal({});
+
+  const testDB2 = {
+    name: 'testName2',
+    replicacheFormatVersion: 2,
+    schemaVersion: 'testSchemaVersion2',
+  };
+
+  expect(await store.putDatabase(testDB2)).to.deep.equal({
+    testName2: testDB2,
+  });
+  expect(await store.getDatabases()).to.deep.equal({
+    testName2: testDB2,
+  });
+});

src/persist/idb-databases-store.ts
@@ -1,7 +1,7 @@
-import {assertNumber, assertObject, assertString} from '../asserts';
+import {assert, assertNumber, assertObject, assertString} from '../asserts';
 import * as kv from '../kv/mod';
 
-const IDB_NAME = 'replicache-dbs';
+export const IDB_DATABASES_DB_NAME = 'replicache-dbs';
 const KEY = 'dbs';
 
 // TODO: make an opaque type
@@ -22,6 +22,7 @@ function assertIndexedDBDatabaseRecord(
   for (const [name, db] of Object.entries(value)) {
     assertString(name);
     assertIndexedDBDatabase(db);
+    assert(name === db.name);
   }
 }
 
@@ -40,7 +41,7 @@ export class IDBDatabasesStore {
   constructor(
     createKVStore: (name: string) => kv.Store = name => new kv.IDBStore(name),
   ) {
-    this._kvStore = createKVStore(IDB_NAME);
+    this._kvStore = createKVStore(IDB_DATABASES_DB_NAME);
   }
 
   putDatabase(db: IndexedDBDatabase): Promise<IndexedDBDatabaseRecord> {
@@ -50,6 +51,13 @@ export class IDBDatabasesStore {
       await write.put(KEY, dbRecord);
       await write.commit();
       return dbRecord;
+    });
+  }
+
+  clear(): Promise<void> {
+    return this._kvStore.withWrite(async write => {
+      await write.del(KEY);
+      await write.commit();
     });
   }
 

src/persist/mod.ts
@@ -1,5 +1,13 @@
 export {persist} from './persist';
 export {startHeartbeats} from './heartbeat';
-export {initClient} from './clients';
+export {
+  initClient,
+  getClient,
+  getClients,
+  updateClients,
+  noUpdates as noClientUpdates,
+} from './clients';
 export {initClientGC} from './client-gc';
-export {IDBDatabasesStore} from './idb-databases-store';
+export {IDB_DATABASES_DB_NAME, IDBDatabasesStore} from './idb-databases-store';
+
+export type {Client, ClientMap} from './clients';

commit ac33faf50ceb379998ad1697305c29d19f32644a

fix: fix benchmarks broken by mutation recovery change (#808)
These were broken by 1538c424e2369c36c5a41c7876f0dad338e59768. 

three fixes.
1. close other indexeddbs we try to recover
2. delete IDBDatabasesStore indexeddb db after each benchmark.  this way mutationRecovery is not try to recover a ton of dbs during benchmarks. 
3. make the indexeddb deletion code in perf tests more robost.  There is no way to wait for an indexeddb close to complete (the api is silently async).  If you call delete when a close is in process, it fails with a onblocked event.  Add code that will delay 100ms and then retry delete after a onblocked event (up to 10 retries).

perf/replicache.ts
@@ -15,6 +15,7 @@ import type {Bencher, Benchmark} from './perf';
 import {range, sampleSize} from 'lodash-es';
 import {makeIdbName} from '../src/replicache';
 import {resolver} from '../src/resolver';
+import {IDB_DATABASES_DB_NAME} from '../src/persist/idb-databases-store';
 
 export function benchmarkPopulate(opts: {
   numKeys: number;
@@ -74,6 +75,7 @@ async function setupPersistedData(
   }
 
   await deleteDatabase(makeIdbName(replicacheName));
+  await deleteDatabase(IDB_DATABASES_DB_NAME);
   // populate store using pull (as opposed to mutators)
   // so that a snapshot commit is created, which new clients
   // can use to bootstrap.
@@ -121,6 +123,7 @@ export function benchmarkStartupUsingBasicReadsFromPersistedData(opts: {
         range(opts.numKeysPersisted),
         opts.numKeysToRead,
       ).map(i => `key${i}`);
+      await deleteDatabase(IDB_DATABASES_DB_NAME);
       bencher.reset();
       const rep = new Replicache({
         name: repName,
@@ -389,10 +392,26 @@ export function benchmarkWriteSubRead(opts: {
 }
 
 function deleteDatabase(name: string): Promise<unknown> {
-  return new Promise((resolve, reject) => {
-    const req = indexedDB.deleteDatabase(makeIdbName(name));
+  const maxBlockedRetry = 10;
+  const retryDelayMs = 100;
+  let retryBlockCount = 0;
+  function delDB(resolve: (_: unknown) => void, reject: (_: unknown) => void) {
+    console.log(name);
+    const req = indexedDB.deleteDatabase(name);
     req.onsuccess = resolve;
-    req.onerror = req.onblocked = req.onupgradeneeded = reject;
+    req.onerror = req.onupgradeneeded = reject;
+    req.onblocked = async event => {
+      retryBlockCount++;
+      if (retryBlockCount > maxBlockedRetry) {
+        reject(event);
+      } else {
+        await sleep(retryDelayMs);
+        await delDB(resolve, reject);
+      }
+    };
+  }
+  return new Promise((resolve, reject) => {
+    delDB(resolve, reject);
   });
 }
 
@@ -401,7 +420,8 @@ async function makeRep<MD extends MutatorDefs>(
   options: Omit<ReplicacheOptions<MD>, 'name'> = {},
 ) {
   const name = `bench${counter++}`;
-  await deleteDatabase(name);
+  await deleteDatabase(makeIdbName(name));
+  await deleteDatabase(IDB_DATABASES_DB_NAME);
   return new Replicache<MD>({
     name,
     pullInterval: null,

src/replicache.ts
@@ -1190,7 +1190,14 @@ export class Replicache<MD extends MutatorDefs = {}> {
           dag.throwChunkHasher,
           assertNotTempHash,
         );
-        await this._recoverMutationsFromPerdag(perdag, database.schemaVersion);
+        try {
+          await this._recoverMutationsFromPerdag(
+            perdag,
+            database.schemaVersion,
+          );
+        } finally {
+          await perdag.close();
+        }
       }
     } catch (e) {
       if (this.closed) {
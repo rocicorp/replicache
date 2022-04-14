5d518d628c5a809ba615dc49017fe224e6a440d6
Erik Arvidsson <erik.arvidsson@gmail.com>
2022-04-14 12:39:28 +0200

    refactor: Add AbortSignal to sleep
    I need it in the near future

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,14 +1,29 @@
+import {AbortError} from './abort-error';
+
 /**
  * Creates a promise that resolves after [[ms]] milliseconds. Note that if you
  * pass in `0` no `setTimeout` is used and the promise resolves immediately. In
  * other words no macro task is used in that case.
+ *
+ * Pass in an AbortSignal to clear the timeout.
  */
-export function sleep(ms: number): Promise<void> {
+export function sleep(ms: number, signal?: AbortSignal): Promise<void> {
   if (ms === 0) {
     return Promise.resolve();
   }
 
-  return new Promise(resolve => {
-    setTimeout(() => resolve(), ms);
+  return new Promise((resolve, reject) => {
+    const id = setTimeout(() => {
+      resolve();
+    }, ms);
+
+    if (signal) {
+      signal.addEventListener('abort', () => {
+        if (id) {
+          clearTimeout(id);
+        }
+        reject(new AbortError('Aborted'));
+      });
+    }
   });
 }
